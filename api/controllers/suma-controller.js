6b7a68d1b405a95dd7d48bb3239a23047f31b30f
Cristian Serey <sereyonose@gmail.com>
2021-01-07 17:15:41 -0300

    authorization

--- api/controllers/suma-controller.js
+++ api/controllers/suma-controller.js
@@ -3,14 +3,14 @@ const cryptoUtil = require('../utils/cryptoUtil');
 
 const sum = async (req, res) => {
     const { n1, n2 } = req.body;
-    const { Authorization } = req.headers
+    const { authorization } = req.headers
     try {
-        const email = cryptoUtil.getEmailFromToken(Authorization);
+        const email = cryptoUtil.getEmailFromToken(authorization);
         await db.insertAction(email, 3);
         res.status(200).send({ suma: n1 + n2 });
     }
     catch (e) {
-        res.status(500).send(JSON.stringify(req.headers));
+        res.status(500).send();
     }
 
 }
